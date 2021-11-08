bf25ca241b1585cae6d7d76107da2e1a9ffdd191
Paweł Dąbrowski <dziamber@gmail.com>
2021-11-08 22:32:16 +0100

    Add support for BCC

--- lib/office_boy/mail.rb
+++ lib/office_boy/mail.rb
@@ -2,7 +2,18 @@ module OfficeBoy
   class Mail
     def self.deliver(template:, attributes:)
       raise Errors::NotDefiniedEmailTemplate unless OfficeBoy.configuration.templates.key?(template)
-    
+      
+      personalization = {
+        to: [{
+          email: attributes[:to_email],
+          name: attributes[:to_name]
+        }],
+        subject: attributes[:subject],
+        dynamic_template_data: attributes[:dynamic_template_data]
+      }
+
+      personalization[:bcc] = [{email: attributes[:bcc_email]}] if attributes[:bcc_email]
+
       response = OfficeBoy::Request.call(
         method_name: :post,
         path: 'mail/send',
@@ -12,16 +23,7 @@ module OfficeBoy
             name: attributes[:from_name]
           },
           template_id: OfficeBoy.configuration.templates[template],
-          personalizations: [
-            {
-              to: [{
-                email: attributes[:to_email],
-                name: attributes[:to_name]
-              }],
-              subject: attributes[:subject],
-              dynamic_template_data: attributes[:dynamic_template_data]
-            }
-          ]
+          personalizations: [personalization]
         }
       )
 
